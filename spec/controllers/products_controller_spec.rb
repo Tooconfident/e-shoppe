bc3c88cc83fb954254c38e89f96856114c7212fd
Ryan Wilkins & Talal Talhouk <pair+tooconfident.ttalhouk@devbootcamp.com>
2016-05-23 19:46:38 -0700

    add tests for product model and for index and show route contollers

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+    it "loads all of the products into @products" do
+      product1, product2 = Product.create!, Product.create!
+      get :index
+      expect(assigns(:products)).to match_array([product1, product2])
+    end
+  end
+
+  describe "GET #show" do
+    it "responds successfully with an HTTP 200 status code" do
+      product3 = Product.create!
+      get :show, :id => 3
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the show template" do
+      product4 = Product.create!
+      get :show, :id => 4
+      expect(response).to render_template("show")
+    end
+
+    it "load the product into @product" do
+      product5 = Product.create!
+      get :show, :id => 5
+      expect(assigns(:product)).to eq(product5)
+    end
+  end
 
 end
