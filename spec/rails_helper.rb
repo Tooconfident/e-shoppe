e9980bf1b1afb83f364731127127b3661734ed0c
Ryan Wilkins & Talal Talhouk <pair+tooconfident.ttalhouk@devbootcamp.com>
2016-05-29 19:22:06 -0700

    Update gems and spec files for travis testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ require 'faker'
 require 'capybara/rails'
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
 
 Capybara.default_driver = :selenium
